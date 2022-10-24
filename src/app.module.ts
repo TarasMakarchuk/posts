3c33fe3cab7145bc8b6200c9824ad00809317d0b
Taras Makarchuk <2278452@gmail.com>
2022-10-24 18:52:21 +0300

    create and connecting to db postgresql, create .env.dist file, remove app.controller.ts and app.service.ts files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      // @ts-ignore
+      dialect: process.env.DB_DIALECT,
+      host: process.env.DB_HOST,
+      port: Number(process.env.PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+  // imports: [
+  //   SequelizeModule.forRoot({
+  //     dialect: 'postgres',
+  //     host: 'localhost',
+  //     port: 5432,
+  //     username: 'postgres',
+  //     password: 'root',
+  //     database: 'posts-nest-course',
+  //     models: [],
+  //     autoLoadModels: true,
+  //   }),
+  // ],
 })
 
 export class AppModule {}
