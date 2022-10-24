60dcc3b60fe1ed80f2b3e46935b4a73f92712baa
Taras Makarchuk <2278452@gmail.com>
2022-10-24 19:31:05 +0300

    add nestjs/config and cross-env npms, create development and productions environment, update start commands in package.json file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
-      // @ts-ignore
-      dialect: process.env.DB_DIALECT,
-      host: process.env.DB_HOST,
-      port: Number(process.env.PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
