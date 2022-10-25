2250b8f4cc253196c50448f64d949a856e20b8d5
Taras Makarchuk <2278452@gmail.com>
2022-10-25 19:37:14 +0300

    add user login, user's email and password validate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "../users/dto/create-user.dto";
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { UserWithThisEmailAlreadyExists } from '../exceptions/userWithThisEmailAlreadyExists';
 import * as bcrypt from 'bcryptjs';
 import { User } from '../users/users.model';
+import { IncorrectUserCredentials } from '../exceptions/incorrectUserCredentials';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +13,8 @@ export class AuthService {
               private jwtService: JwtService) {}
 
   async login(dto: CreateUserDto) {
+    const user = await this.validateUser(dto);
+    return this.generateToken(user);
   }
 
   async registration(dto: CreateUserDto) {
@@ -26,7 +29,7 @@ export class AuthService {
     return this.generateToken(user);
   };
 
-  private generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = {
       email: user.email,
       id: user.id,
@@ -37,4 +40,14 @@ export class AuthService {
       token: this.jwtService.sign(payload)
     };
   };
+
+  private async validateUser(dto: CreateUserDto) {
+    const user = await this.usersService.getUserByEmail(dto.email);
+    const passwordEqual = await bcrypt.compare(dto.password, user.password);
+    if (user && passwordEqual) {
+      return user;
+    }
+
+    throw new IncorrectUserCredentials();
+  }
 }
