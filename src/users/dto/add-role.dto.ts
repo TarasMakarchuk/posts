e8b0efc0e45a69d8421891825a99676003cd97ca
Taras Makarchuk <2278452@gmail.com>
2022-10-26 18:59:49 +0300

    add swagger to add-role.dto.ts and ban-user.dto.ts files, update README.md file

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class AddRoleDto {
+  @ApiProperty({ example: 'User', description: 'Should be uppercase'})
   readonly value: string;
+
+  @ApiProperty({ example: 31, description: 'Should be number'})
   readonly userId: number;
 }
--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class BanUserDto {
+  @ApiProperty({ example: 31, description: 'Should be number'})
   readonly userId: number;
+
+  @ApiProperty({ example: 'For bad behavior', description: 'Ban reason'})
   readonly banReason: string;
 }
