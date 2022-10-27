25e4690366202976c847130e0cf6c92bd9f0cc5f
Taras Makarchuk <2278452@gmail.com>
2022-10-27 19:42:43 +0300

    merge

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class AddRoleDto {
+  @ApiProperty({ example: 'User', description: 'Should be uppercase'})
   @IsString({ message: 'It should be a string' })
   readonly value: string;
 
+  @ApiProperty({ example: 31, description: 'Should be number'})
   @IsNumber({}, { message: 'It should be a number' })
   readonly userId: number;
 }
